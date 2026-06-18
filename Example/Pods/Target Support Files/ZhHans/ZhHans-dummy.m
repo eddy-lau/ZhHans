95a520a5d104fd5e8a41f20fdef3eedfe693745d
Eddie Lau <eddie@touchutility.com>
2026-06-18 14:26:56 +0800

    bump 1.8.1: remove dead 3-char entries from extraTable4

---
+++ Example/Pods/Target Support Files/ZhHans/ZhHans-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZhHans : NSObject
+@end
+@implementation PodsDummy_ZhHans
+@end
---
+++ Example/Pods/Target Support Files/ZhHans/ZhHans-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSString+ZhHans.h"
+#import "T2S2.h"
+#import "ZhHans.h"
+#import "ZhHansHelper.h"
+
+FOUNDATION_EXPORT double ZhHansVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZhHansVersionString[];
+
